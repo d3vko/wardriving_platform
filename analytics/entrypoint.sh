f2b83748a7456a98b1290d6c3a38c9a2f3805696
Adrian González Pardo <gozapaadr@gmail.com>
2026-03-23 11:51:07 -0600

    fix: entrypoint deploy queries analytics

--- analytics/entrypoint.sh
+++ analytics/entrypoint.sh
@@ -2,8 +2,17 @@
 set -eu
 
 APP_DIR="/usr/src/app"
-TEMPLATE="${APP_DIR}/queries/source.yml.tpl"
-TARGET="${APP_DIR}/queries/source.yml"
+if [ -d "${APP_DIR}/build/queries" ]; then
+  QUERIES_DIR="${APP_DIR}/build/queries"
+elif [ -d "${APP_DIR}/queries" ]; then
+  QUERIES_DIR="${APP_DIR}/queries"
+else
+  echo "No queries directory found in ${APP_DIR}" >&2
+  exit 1
+fi
+
+TEMPLATE="${QUERIES_DIR}/source.yml.tpl"
+TARGET="${QUERIES_DIR}/source.yml"
 
 : "${LATITUDE__POSTGRES_DB:=postgres}"
 : "${LATITUDE__POSTGRES_USER:=postgres}"
